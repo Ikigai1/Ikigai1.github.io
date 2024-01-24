a3a2d4d45d2914d2546b852c3334129322af3f9c
Ikigai1 <49373340+Ikigai1@users.noreply.github.com>
2024-01-24 01:42:54 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ let minSell = 0;
 function calculateMin() {
     debug.log("clicked!");
     var quota = document.getElementById("quota").value;
-    var moneyWanted = document.getElementById("money");
+    var moneyWanted = document.getElementById("money").value;
     // check if the entered values are numbers
     if (!isNaN(Number(quota)) && !isNaN(Number(money))) {
       minSell = (5 * moneyWanted + quota) / 6; 
-      document.querySelector("h2").textContent = "Minimum scrap to sell: ${minSell}";
+      document.querySelector("h2").textContent = `Minimum scrap to sell: ${minSell}`;
     }
   }
